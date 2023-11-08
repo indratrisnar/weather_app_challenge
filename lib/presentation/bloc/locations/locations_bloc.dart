a7ab610015aeb7743eed85225cd873bed56bb8e7
indratrisnar <trisnaindra1998@gmail.com>
2023-11-08 23:21:30 +0700

    reduce delay

--- lib/presentation/bloc/locations/locations_bloc.dart
+++ lib/presentation/bloc/locations/locations_bloc.dart
@@ -10,7 +10,7 @@ class LocationsBloc extends Bloc<LocationsEvent, LocationsState> {
     on<OnGetLocations>((event, emit) async {
       emit(LocationsLoading());
       final result = await weatherSource.getLocationsWeather(event.cities);
-      await Future.delayed(const Duration(milliseconds: 1500));
+      await Future.delayed(const Duration(milliseconds: 300));
       result.fold(
         (error) => emit(LocationsError(error)),
         (data) => emit(LocationsLoaded(data)),
